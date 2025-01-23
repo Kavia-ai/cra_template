ee0e375980f1200da8e2bfe489c539a97d7a1f24
Zoltan Kuscsik <zoltan@s57.io>
2025-01-23 09:01:57 +0100

    Updated template

--- template/src/App.js
+++ template/src/App.js
@@ -7,9 +7,17 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Kavai is building this App.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
-      <header>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
